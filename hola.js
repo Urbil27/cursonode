14b34c6e4a5b0c7660177fa15b42ea36d8154051
Urbil <you@example.com>
2020-04-11 15:29:08 +0200

    primer commit

---
+++ hola.js
@@ -0,0 +1,7 @@
+var http = require("http");
+var manejador = function(solicitud, respuesta){
+console.log("Hola mundo");
+respuesta.end();
+}
+var servidor = http.createServer(manejador);
+servidor.listen(8080);
